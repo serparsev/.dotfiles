5cd92bae06008c35409b0f8b501712cd1246baaf
serparsev <41074845+serparsev@users.noreply.github.com>
2022-10-02 19:12:05 +0200

    Install git

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,16 @@
 sudo apt update
 sudo apt upgrade -y
 
-SCRIPTS_PATH=~/.dotfiles/scripts
-
-"$SCRIPTS_PATH"/.aptinstall.sh
-"$SCRIPTS_PATH"/.programs.sh
+# Install git
+sudo apt install -y git
 
 # clone this repo
 git clone https://github.com/serparsev/.dotfiles.git ~/.dotfiles
 
+SCRIPTS_PATH=~/.dotfiles/scripts
+
+"$SCRIPTS_PATH"/.aptinstall.sh
+"$SCRIPTS_PATH"/.programs.sh
 "$SCRIPTS_PATH"/.symlink.sh
 
 # See our bash changes
